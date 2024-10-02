37a6e6f1dcbb10111e5c11a5a9df4a5eba0ae6dc
skyliness1 <123408514@qq.com>
2024-10-03 03:12:05 +0800

    Site updated: 2024-10-03 03:12:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/01/hello-world/"];function toRandomPost(){
+var posts=["2024/10/01/hello-world/","2024/10/03/标准光照模型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
