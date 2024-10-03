5005ac3bb355a68e28f99fca9e652d24332156ba
skyliness1 <123408514@qq.com>
2024-10-03 16:44:23 +0800

    Site updated: 2024-10-03 16:44:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/01/hello-world/","2024/10/03/标准光照模型/"];function toRandomPost(){
+var posts=["2024/10/01/hello-world/","2024/10/03/标准光照模型/","2024/10/03/渲染流水线概述/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
