e1efd31d6476d006d4984d85d1a02911aba55dbb
skyliness1 <123408514@qq.com>
2024-10-04 01:17:20 +0800

    Site updated: 2024-10-04 01:17:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/01/hello-world/","2024/10/03/一种工程命名的规范/","2024/10/04/渲染流水线概述/","2024/10/03/标准光照模型/"];function toRandomPost(){
+var posts=["2024/10/01/hello-world/","2024/10/04/渲染流水线概述/","2024/10/03/一种工程命名的规范/","2024/10/03/标准光照模型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
